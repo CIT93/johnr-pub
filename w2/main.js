2685be59fe37c0d2a5f667dd09f12e5b4342f8e7
John Rodriguez <jarodriguez53@my.scccd.edu>
2024-01-16 09:58:28 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,13 @@
+const myHouseholdSize = 4;
+const myHomeSize = 4;
+const myFoodChoices = 8;
+const myWaterUsage = 2;
+const myHomeGadgets = 10;
+const myWasteOutput = 20;
+const myRecycledWaste = 12;
+const myTravelMiles = 10;
+
+const myTotalCFootprint = myHouseholdSize + myHomeSize + myFoodChoices + myWaterUsage + myHomeGadgets + myWasteOutput + myRecycledWaste + myTravelMiles
+
+const myHeading2 = document.querySelector("h2");
+myHeading2.textContent = myTotalCFootprint
